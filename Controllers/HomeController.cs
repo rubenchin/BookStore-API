d9f33b9b90c7f8aeb2035525efd922e57d33ec24
Ruben López <Ruben Lpez@DESKTOP-I7HESU3>
2020-05-04 16:54:36 -0400

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookStore_API.Contracts;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore_API.Controllers
+{
+    /// <summary>
+    /// This is a test controller
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        private readonly ILoggerService _logger;
+        public HomeController(ILoggerService logger)
+        {
+            _logger = logger;
+        }
+        /// <summary>
+        /// Get values
+        /// </summary>
+        /// <returns></returns>
+        // GET: api/Home
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            _logger.LogInfo("Accessed from Home Controller");
+            return new string[] { "value1", "value2" };
+        }
+        /// <summary>
+        /// Get Value
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        // GET: api/Home/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            _logger.LogInfo("Got a value");
+
+            return "value";
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="value"></param>
+        // POST: api/Home
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+            _logger.LogInfo("This is an error");
+
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="value"></param>
+        // PUT: api/Home/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            _logger.LogInfo("This is a warning");
+
+        }
+    }
+}
